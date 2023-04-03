b226c3c9f227fc711537e23e52466cf72acefb0d
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-04-03 21:32:00 +0600

    Задание 2

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ console.log(document.getElementById('string-4').innerText)
 console.log(document.getElementById('string-1').innerText)
 
 
+const text = document.getElementsByClassName('element')
+console.log(text[0].style.color = 'red')
+console.log(text[1].style.color = 'red')
+console.log(text[2].style.color = 'red')
+console.log(text[3].style.color = 'green')
+console.log(text[4].style.color = 'green')
+console.log(text[5].style.color = 'green')
 
