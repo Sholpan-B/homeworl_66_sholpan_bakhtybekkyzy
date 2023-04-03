3b310a1bb26022f5233fd7e00bbcacf6fad2db21
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-04-03 21:30:03 +0600

    Задание 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 console.log(document.getElementById('string-3').innerText)
 console.log(document.getElementById('string-5').innerText)
 console.log(document.getElementById('string-2').innerText)
@@ -7,37 +6,4 @@ console.log(document.getElementById('string-4').innerText)
 console.log(document.getElementById('string-1').innerText)
 
 
-const text = document.getElementsByClassName('element')
-console.log(text[0].style.color = 'red')
-console.log(text[1].style.color = 'red')
-console.log(text[2].style.color = 'red')
-console.log(text[3].style.color = 'green')
-console.log(text[4].style.color = 'green')
-console.log(text[5].style.color = 'green')
-
-
-// const container = document.getElementById('container');
-// const colors = ['0','green', 'red', 'blue', 'brown', 'yellow'];
-// for(let i = 1; i<=5; i++) {
-//     const div = document.createElement('div');
-//     div.classList.add('element');
-//     div.textContent = `Element ${i}` 
-//     div.style.color = colors[i];
-//     container.appendChild(div)
-// }
-
-
-
-const container = document.getElementById('container');
-const colors = ['green', 'red', 'blue', 'brown', 'yellow'];
-for(let i = 1; i<=5; i++) {
-    const div = document.createElement('div');
-    div.classList.add('element');
-    div.textContent = 'Element '
-    const span = document.createElement('span')
-    span.textContent = i
-    span.style.color = colors[i-1];
-    div.appendChild(span)
-    container.appendChild(div)
-}
 
